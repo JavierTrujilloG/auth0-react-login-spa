100ded24a5541596460838801595e6e809291c41
Javier Trujillo <javi.trujillo@auth0.com>
2023-08-11 08:24:40 +0200

    first commit

---
+++ src/auth.js
@@ -0,0 +1,94 @@
+import React, { useCallback, useMemo, createContext, useContext, useState } from "react";
+import auth0 from "auth0-js";
+import { getConfig } from "./config";
+
+const DATABASE_CONNECTION = "Username-Password-Authentication";
+
+export const AuthContext = createContext({
+  loginWithUsernamePassword: (username, password) => {},
+  loginWithGoogle: () => {},
+  signUp: (username, password, name) => {}
+});
+
+const { domain, clientId } = getConfig();
+
+export function AuthProvider({ children }) {
+  const webAuth = useMemo(
+    () =>
+      new auth0.WebAuth({
+        domain,
+        clientID: clientId,
+        //redirectUri: process.env.REACT_APP_AUTH0_REDIRECT_URI,
+        responseType: "code",
+      }),
+    []
+  );
+  const [ existingUserError, setExistingError] = useState("");
+  const loginWithUsernamePassword = useCallback((username, password) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const stateParam = urlParams.get("state") || "";
+    return new Promise(
+      (resolve, reject) => {
+        webAuth.login(
+          {
+            username,
+            password,
+            realm: DATABASE_CONNECTION,
+            state: stateParam,
+          },
+          (error, result) => {
+            if (error) {
+              reject(error);
+              return;
+            }
+            resolve(result);
+          }
+        );
+      },
+      [webAuth]
+    );
+  });
+  const loginWithGoogle = useCallback(() => {
+    webAuth.authorize({ connection: "google-oauth2" });
+  }, [webAuth]);
+
+  const signUp = useCallback(
+    (username, password, name) => {
+      return new Promise((resolve, reject) => {
+        webAuth.signup(
+          {
+            connection: DATABASE_CONNECTION,
+            password: password,
+            email: username,
+            name,
+          },
+          (error, result) => {
+            if (error) {
+              reject(error);
+              return;
+            }
+            resolve(result);
+          }
+        );
+      });
+    },
+    [webAuth]
+  );
+
+  const value = useMemo(
+    () => ({
+      loginWithUsernamePassword,
+      loginWithGoogle,
+      signUp
+    }),
+    [loginWithGoogle, loginWithUsernamePassword, signUp]
+  );
+
+  return (
+    <AuthContext.Provider value={value}>
+        {children}
+    </AuthContext.Provider>
+  );
+}
+
+// export const useAuth = () => useContext(AuthContext);
---
+++ src/login.js
@@ -0,0 +1,141 @@
+import React, { useState, useContext } from "react";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import { AuthContext } from "./auth";
+
+const LoginSchema = yup.object().shape({
+  username: yup.string().required("Please enter your email address."),
+  password: yup.string().required("Please enter your password."),
+});
+
+const styles = {}; // TODO
+
+export default function Login() {
+  const [authError, setAuthError] = useState(null);
+  const validationOpt = { resolver: yupResolver(LoginSchema) };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm(validationOpt);
+
+  const {
+    loginWithUsernamePassword,
+    loginWithGoogle,
+    loginWithFacebook
+  } = useContext(AuthContext);
+
+
+  async function onSubmit(data) {
+    try {
+      await loginWithUsernamePassword(data.username, data.password);
+    } catch (err) {
+      setAuthError(err.description);
+      console.log(err);
+    }
+  }
+
+  return (
+    <>
+      <main id={styles.container}>
+        <div>
+          <h1 id={styles.header}>Login</h1>
+        </div>
+        <div id={styles.socialWrapper}>
+          <button
+            type="button"
+            id={styles.google}
+            onClick={async (e) => {
+              e.preventDefault();
+              loginWithGoogle();
+            }}
+          >
+            Login with Google
+          </button>
+
+          <button
+            type="button"
+            id={styles.facebook}
+            onClick={async (e) => {
+              e.preventDefault();
+              loginWithFacebook();
+            }}
+          >
+            Login with Facebook
+          </button>
+        </div>
+
+        <form onSubmit={handleSubmit(onSubmit)} id={styles.loginForm}>
+          <label htmlFor="username">
+            <input
+              type="text"
+              {...register("username")}
+              placeholder="Email"
+              name="username"
+              id="username"
+              autoComplete="username"
+              className={
+                authError || errors.username ? styles.inputWithError : ""
+              }
+            />
+          </label>
+          <label htmlFor="password">
+            <input
+              type="password"
+              {...register("password")}
+              placeholder="Password"
+              name="password"
+              id="password"
+              autoComplete="current-password"
+              className={
+                authError || errors.password ? styles.inputWithError : ""
+              }
+            />
+          </label>
+
+          {authError && <p id={styles.authError}>{authError}</p>}
+          {errors.username && (
+            <p id={styles.authError}>{errors.username.message}</p>
+          )}
+          {errors.password && (
+            <p id={styles.authError}>{errors.password.message}</p>
+          )}
+          <button
+            type="submit"
+            className={authError ? styles.submitWithError : ""}
+          >
+            Log in
+          </button>
+        </form>
+        <div id={styles.forgotPasswordWrapper}>
+          <button
+            type="button"
+            className="link-button"
+            onClick={(e) => {
+              e.preventDefault();
+              //goToResetPassword(); TODO
+            }}
+          >
+            Forgot your password?
+          </button>
+        </div>
+        <div id={styles.registrationWrapper}>
+          <p>
+            Don't have an account?
+            <button
+              type="button"
+              className="link-button"
+              onClick={(e) => {
+                e.preventDefault();
+                // goToSignUp(); TODO
+              }}
+            >
+              Register an account
+            </button>
+          </p>
+        </div>
+      </main>
+    </>
+  );
+}
